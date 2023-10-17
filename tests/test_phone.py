3f37f6fb8fef058f94fc8e89a5b5a373851bf27f
ElenaChudinova <maknali@yandex.ru>
2023-10-17 15:45:53 +0300

    new file and correct old file

---
+++ tests/test_phone.py
@@ -0,0 +1,20 @@
+import pytest
+from src.phone import Phone
+
+@pytest.fixture
+def phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test_number_of_sim(phone):
+    with pytest.raises(ValueError):
+        phone.number_of_sim = -1
+        phone.number_of_sim = 0
+        phone.number_of_sim = 1.2
+
+def test__repr__(phone):
+    assert repr(phone) == "Phone('iPhone 14', 120000, 5)"
+
+
+def test__add__(phone):
+    assert phone + phone == 10
